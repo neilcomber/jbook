8ae90691a3e2a0aff7cc3a99a294758184fd6f07
Neil Comber <neilcomber@gmail.com>
2023-11-01 06:17:08 +0000

    fixed color problem in text editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,14 +1,45 @@
+import './text-editor.css'
 import MDEditor from "@uiw/react-md-editor";
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 const TextEditor: React.FC = () => {
 
-  const [value, setValue] = useState("");
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("Header");
 
-  return <div>
-    <MDEditor value={value} onChange={setValue} />
-    <MDEditor.Markdown source={value} style={{whiteSpace: 'pre-wrap'}} />
-  </div>;
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (ref.current && event.target && ref.current.contains(event.target as Node)) {
+        
+        return;
+      }
+     
+
+      setEditing(false)
+    };
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    }
+  }, [])
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor className="text-editor" value={value} onChange={(v)=> setValue(v || "") } />
+      </div>
+    )
+  }
+
+  return (<div className="text-editor card" onClick={() => {
+    setEditing(true)
+  }}>
+    <div className="card-content">
+      <MDEditor.Markdown source={value} />
+      </div>
+  </div>);
 };
 
 export default TextEditor;
