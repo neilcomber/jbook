1669ad04c072029bbc23226b686a06d6821d2641
Neil Comber <neilcomber@gmail.com>
2023-11-02 12:14:09 +0000

    maunally testing redux store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,47 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import { ActionType } from './action-types';
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
 
+
+// some manual testing:
+
+
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_BEFORE, 
+//   payload: {
+//     id: null, 
+//     type: 'code'
+//   }
+// })
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_BEFORE, 
+//   payload: {
+//     id: null, 
+//     type: 'code'
+//   }
+// })
+// store.dispatch({
+//   type: ActionType.INSERT_CELL_BEFORE, 
+//   payload: {
+//     id: null, 
+//     type: 'code'
+//   }
+// })
+
+// console.log(store.getState());
+
+
+// const id = store.getState().cells.order[1];
+
+// store.dispatch({
+//   type: ActionType.UPDATE_CELL, 
+//   payload: {
+//     id,
+//     content: 'down'
+//   }
+// })
+
+// console.log(store.getState());
