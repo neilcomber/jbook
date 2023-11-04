a5457e4aa76ece8ea20638078f6ca2c96f832f85
Neil Comber <neilcomber@gmail.com>
2023-11-04 09:53:02 +0000

    morning time

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -2,18 +2,33 @@ import './add-cell.css';
 import { useActions } from '../hooks/use-actions'
 
 interface AddCellProps {
-  nextCellId: string;
+  nextCellId: string | null;
+  forceVisible?: boolean;
 }
 
-const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+const AddCell: React.FC<AddCellProps> = ({ forceVisible, nextCellId }) => {
 
   const { insertCellBefore } = useActions();
   
 
   return (
-    <div>
-      <button onClick={()=>{insertCellBefore(nextCellId, 'text')}}>Text</button>
-      <button onClick={()=>{insertCellBefore(nextCellId, 'code')}}>Code</button>
+    <div className={`${forceVisible && 'force-visible'} add-cell`}>
+      <div className="add-buttons">
+        <button className="button is-rounded is-primary is-small" onClick={() => { insertCellBefore(nextCellId, 'code') }}>
+          <span className="icon is-small">
+            <i className="fas fa-plus" />
+          </span>
+          <span>Code</span>
+          </button>
+        <button className="button is-rounded is-primary is-small" onClick={() => { insertCellBefore(nextCellId, 'text') }}>
+          <span className="icon is-small">
+            <i className="fas fa-plus" />
+          </span>
+          <span>Text</span>
+          </button>
+        
+        </div>
+      <div className="divider"></div>
     </div>
   )
 
